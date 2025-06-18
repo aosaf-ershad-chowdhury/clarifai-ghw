cde32621caf066304b300b358de93dfc5c4c0677
aosaf-e-c <aosaf@connect.hku.hk>
2025-06-18 22:51:16 +0600

    Model Call

---
+++ test.py
@@ -0,0 +1,8 @@
+from clarifai.client.model import Model
+
+prompt = "Build me a simple agent with Clarifai Python SDK that can answer questions about the weather."
+
+model_url="https://clarifai.com/qwen/qwenLM/models/Qwen3-14B"
+model_prediction = Model(url=model_url, pat="11d540cd9603427a82ee0572e86c3521").predict_by_bytes(prompt.encode())
+
+print(model_prediction.outputs[0].data.text.raw)
